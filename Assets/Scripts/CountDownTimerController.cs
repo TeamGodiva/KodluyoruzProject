23612ee87ab16b95fdc26387393488986647dfe4
kursaterdogan <soullreaver@icloud.com>
2020-10-03 00:34:44 +0300

    Update CountDownTimerController.cs

--- Assets/Scripts/CountDownTimerController.cs
+++ Assets/Scripts/CountDownTimerController.cs
@@ -1,31 +1,29 @@
-﻿using System;
-using DG.Tweening;
-using JetBrains.Annotations;
+﻿using DG.Tweening;
 using TMPro;
 using UnityEngine;
 using UnityEngine.UI;
 
 public class CountDownTimerController : MonoBehaviour
 {
+    [SerializeField] private GameController gameController;
     [SerializeField] private Image frontCircle;
     [SerializeField] private RectTransform arrowImage;
     [SerializeField] private TextMeshProUGUI secondText;
-    
+
     private Tween _tweenOfCircleCalculate;
-    private Tween _tweenOfArrowRotation; 
-    
+    private Tween _tweenOfArrowRotation;
+
     private float _tweenTime;
     private bool _isTimeStopped;
     private float _timeLeft;
 
-   
 
     public float TimeLeft
     {
         get => _timeLeft;
         set => _timeLeft = value;
     }
-    
+
     void Update()
     {
         if (!_isTimeStopped)
@@ -34,7 +32,7 @@ public class CountDownTimerController : MonoBehaviour
             secondText.text = (_timeLeft).ToString("0");
             if (_timeLeft <= 0)
             {
-                _timeLeft = 0;
+                gameController.RestartGame();
             }
         }
     }
@@ -45,32 +43,31 @@ public class CountDownTimerController : MonoBehaviour
         _tweenTime = _timeLeft;
         StartArrowRotation();
         StartFrontCircle();
-        
     }
-    
+
     private void StartFrontCircle()
     {
         //Calculate the ratio of remaining time to 1-0 
         _tweenOfCircleCalculate = frontCircle.DOFillAmount(0, _tweenTime).From(1)
-                                                            .SetEase(Ease.Linear)
-                                                            .SetRelative();
+            .SetEase(Ease.Linear)
+            .SetRelative();
     }
 
     private void StartArrowRotation()
     {
         //Calculate the ratio of remaining time to 360 degrees
         _tweenOfArrowRotation = arrowImage.DORotate(new Vector3(0, 0, -360f), _tweenTime, RotateMode.FastBeyond360)
-                                                    .SetEase(Ease.Linear)
-                                                    .SetRelative();
+            .SetEase(Ease.Linear)
+            .SetRelative();
     }
 
     public void ChangeTimerState()
     {
         _isTimeStopped = !_isTimeStopped;
         _tweenOfCircleCalculate.TogglePause();
-        _tweenOfArrowRotation.TogglePause();//???
+        _tweenOfArrowRotation.TogglePause(); //???
     }
-    
+
     public void LoadTimer()
     {
         _isTimeStopped = false;
@@ -78,7 +75,7 @@ public class CountDownTimerController : MonoBehaviour
         StartArrowRotation();
         StartFrontCircle();
         ChangeTimerState();
-        arrowImage.rotation=Quaternion.Euler(new Vector3(0,0,0));
+        arrowImage.rotation = Quaternion.Euler(new Vector3(0, 0, 0));
     }
 
     public void ResetTimer()
