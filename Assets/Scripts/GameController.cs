8981a7d0f68213a98b136f78d11052a99e1ccc09
kursaterdogan <soullreaver@icloud.com>
2020-10-03 02:40:37 +0300

    Update GameController.cs

--- Assets/Scripts/GameController.cs
+++ Assets/Scripts/GameController.cs
@@ -33,10 +33,8 @@ public class GameController : MonoBehaviour
 
     private void ActivateCompleteParticleSystem()
     {
-        //TODO bu nasıl isimdir yiğidim.
         partSystem.SetActive(true);
     }
-
 
     private void DeActiveParticle()
     {
